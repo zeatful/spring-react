a760c21c4b8ef6bb09a15287ad8e7efb87458fa3
zeatful <zeatful@users.noreply.github.com>
2018-05-12 18:36:56 -0400

    update webpack configuration, update pom plugins to properly include bundle.js in the final jar

--- src/main/java/com/zeatful/springreact/SpringReactApplication.java
+++ src/main/java/com/zeatful/springreact/SpringReactApplication.java
@@ -2,11 +2,27 @@ package com.zeatful.springreact;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 
 @SpringBootApplication
 public class SpringReactApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringReactApplication.class, args);
+	}
+
+	@Bean
+	public RouterFunction<ServerResponse> routes(){
+		return RouterFunctions.route(
+				GET("/"),
+				request -> ServerResponse.ok().body(BodyInserters.fromResource(new ClassPathResource("public/index.html")))
+		);
 	}
 }
--- src/test/java/com/zeatful/springreact/SpringReactApplicationTests.java
+++ src/test/java/com/zeatful/springreact/SpringReactApplicationTests.java
@@ -13,4 +13,6 @@ public class SpringReactApplicationTests {
 	public void contextLoads() {
 	}
 
+
+
 }
